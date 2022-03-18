41087cf3b22b8a9cfba652133a4fbff5ff2dea87
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2022-03-17 23:40:17 -0500

    Added libraries, basic mechanum drive, subsystems and commands
    Added libraries for the CTRE Phoenix devices and REV SparkMAX motor controllers. This will be used to communicate over the CAN bus.
    
    Added a build step with GitHub Actions to sanity check builds.
    
    Added all subsystems and a few commands to get started with current mechanisms.
    
    Minor changes:
    - Set all line endings to LF from CRLF
    - Set indent size to 4 spaces instead of 2
    - Cleaned up RobotBuilder boilerplace
    - Linted all files

---
+++ src/main/java/frc/robot/commands/MechanumDrive.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.MechanumDriveTrain;
+
+public class MechanumDrive extends CommandBase {
+
+    private final MechanumDriveTrain m_mechDriveTrain;
+
+    private final Joystick driveJoystick = new Joystick(0);
+
+    public MechanumDrive(MechanumDriveTrain subsystem) {
+        m_mechDriveTrain = subsystem;
+        addRequirements(m_mechDriveTrain);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        m_mechDriveTrain.drivetrain(driveJoystick.getX(), driveJoystick.getY(), driveJoystick.getZ());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
